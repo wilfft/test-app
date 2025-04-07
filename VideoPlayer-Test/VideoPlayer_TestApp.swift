bf3630ad2427112108bf379a189f53945c7e0573
william.moraes.glo <william.moraes.3@globalhitss.com.br>
2025-04-07 09:28:51 -0300

    Initial Commit

---
+++ VideoPlayer-Test/VideoPlayer_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_TestApp.swift
+//  VideoPlayer-Test
+//
+//  Created by William Moraes on 07/04/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
